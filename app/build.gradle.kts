5665772836a4052b91e3a3ef8bb0becdf65b628f
IstarVin <ajdeleverio20@gmail.com>
2026-03-27 11:07:25 +0800

    chore: Update versionCode and versionName to 11 and 1.2.0 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.istarvin.skinscriptinstaller"
         minSdk = 30
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
