626bd2e6b14eb2a28a4891329108fedd80944974
IstarVin <ajdeleverio20@gmail.com>
2026-03-28 14:45:38 +0800

    chore: Update versionCode to 17 and versionName to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.istarvin.skinscriptinstaller"
         minSdk = 30
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.4.1"
+        versionCode = 17
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
